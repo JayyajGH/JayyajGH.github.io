492f09caeec00de81c9c84c79761bda6d86d0478
Jason Harris <jason.harris@endsleigh.co.uk>
2019-02-27 15:14:43 +0000

    Update

--- my-script.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  $('.js-trigger').click(function () {
-      $(this).toggleClass('hamburger--close');
-      $('.site-nav').toggleClass('is-active');
-      $('.body').toggleClass('is-overflowhidden');
-    });
-});
